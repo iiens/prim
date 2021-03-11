03f1ce6c541b722c1bc06a55656f5fd165d77210
antoine.man <antoine.man@ensiie.fr>
2021-03-11 22:28:55 +0100

    Tâche A.2 Create enum CaseType and update map.h

---
+++ headers/data/caseType.h
@@ -0,0 +1,37 @@
+/*!
+ * \file caseType.h
+ * \author Antoine Man
+ * \version 0.1
+ * \date 11/03/2021
+ * \see map.h
+ *
+ * Contains all the type that a case can contain.
+ *
+ */
+
+#ifndef PRIM_CASETYPE_H
+#define PRIM_CASETYPE_H
+
+/*!
+ * \typedef CaseType: rename for enum caseType
+ * \enum CaseType caseType.h "headers/data/caseType.h"
+ * \brief all case type
+ *
+ * Contains all type that a case can contain.
+ * Each case can contain 4 elements types :
+ * a part of the production line
+ * a resource case
+ * an empty case
+ * a transdimensional gate
+ *
+ * 3 resources is produced from a resource case every 10 turns
+ *
+ */
+typedef enum caseType {
+    CASE_VIDE = 0, //!< a case that contain nothing
+    CASE_GATE = 1, //!< a case that contain the transdimensional gate
+    CASE_SOURCE = 2, //!< a case that contain resources
+    CASE_MACHINE = 3, //!< a case that contain a machine
+} caseType; //!< Case type
+
+#endif //PRIM_CASETYPE_H
