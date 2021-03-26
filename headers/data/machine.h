0f70689aefd9c51bfb4ff2446d307b791b3f0ee8
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2021-03-26 16:13:52 +0100

    Tâche B - extern again

--- headers/data/machine.h
+++ headers/data/machine.h
@@ -75,56 +75,6 @@
         int capacity; //!< -1 it don't have capacity
     } Machine; //!< Machine
 
-    const MachineInfo machine_list[] = {
-            {
-                    MS_COLLECTOR,200,20,1,500,200,60,200,
-                    "The collector sends a resource produced by the source to the "
-                    "neighboring cell indicated by its orientation. It must be placed "
-                    "on a square adjacent to that of the source",
-                    1, .effect = {
-                    {{
-                             .mode = UPGRADE,
-                             .onOther = true,
-                             .what.machine = MS_COLLECTOR,
-                             .modifier_E = -10,
-                             .modifier_DD = -1,
-                             .min_cost_E = 10,
-                             .min_cost_DD = 1,
-                             .description = "Le coût de construction des collecteurs diminue de 10E et 1DD (minimum 10E\n"
-                                            "et 1DD)."
-                     },}
-                    }
-            },
-            {MS_CONVEYOR_BELT,60,20,1,-1,-1,60,200,
-             "A carpet has one exit and three entrances, the resource or waste exits"
-             " 1 turn after entering the conveyor belt",
-             0, .effect = {{
-                                   .mode = UPGRADE,
-                                   .onOther = true,
-                                   .what.machine = MS_COLLECTOR,
-                                   .modifier_E = -10,
-                                   .modifier_DD = -1,
-                                   .min_cost_E = 10,
-                                   .min_cost_DD = 1,
-                                   .description = "Le coût de construction des collecteurs diminue de 10E et 1DD (minimum 10E\n"
-                                                  "et 1DD)."
-                           },}
-            },
-            {MS_CROSS,160,20,1,-1,-1,60,200,
-             "A cross is a set of two juxtaposed conveyor belts, allowing two "
-             "lines of conveyor belts to cross. The cross has 2 inlets and 2 outlets "
-             "which depend on the orientation of the cross",
-             0, .effect = {}},
-            {MS_RECYCLING_CENTER,500,40,1,1500,100,100,500,
-             "The recycling center is used to store waste. The recycling center"
-             " has 4 entrances and 0 exits. At the start of the game, a recycling "
-             "center can store up to 50 pieces of waste",
-             1, .effect = {}},
-            {MS_JUNKYARD,100,100,1,200,600,100,200,
-             "The recycling center turns waste into a resource. Like the conveyor belt,"
-             " the center has 3 entrances and one exit. The center can store up to 100 "
-             "waste",
-             1, .effect = {}
-            },
-    }; //!< Array with information about all machine
+    extern const MachineInfo machine_list[];
+
 #endif //PRIM_MACHINE_H
