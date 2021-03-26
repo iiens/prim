c370ab0c8b718ff69764e3789ade5442088aed52
ZEBRIR Ramzy <ramzyz125@hotmail.fr>
2021-03-26 16:03:29 +0100

    Tâche B.4 Ajout de la liste du personnel et des machines

--- headers/data/staff.h
+++ headers/data/staff.h
@@ -26,6 +26,82 @@
         Effect effects[20]; //!< array which contains the target of the effect
     } Staff; //!< information about staff
 
-    extern const Staff staff_list[]; //!< Array of the staff
+const Staff staff_list[] = {
+        {1, "Fetia Bannour", 100, 30, "The cost of constructing collectors"
+                                      "decreases by 10EE and 1DD ", .effects = {
+                {
+                        .mode = CONSTRUCTION,
+                        .onOther = false,
+                        .what.machine = MS_COLLECTOR,
+                        .modifierE = -10,
+                        .modifierDD = -1,
+                        .min_costE = 10,
+                        .min_costDD = 1,
+                        .description = "The cost of constructing collectors decreases by 10EE and 1DD (min 10EE\n"
+                                       "and 1DD)."
+                },
+
+        }},
+        {2, "Kevin Goilard", 100, 30, "The cost of constructing conveyor belt"
+                                      "decreases by 3EE and 1DD ", .effects = {
+                {
+                        .mode = CONSTRUCTION,
+                        .onOther = false,
+                        .what.machine = MS_CONVEYOR_BELT,
+                        .modifierE = -3,
+                        .modifierDD = -1,
+                        .min_costE = 3,
+                        .min_costDD = 1,
+                        .description = "The cost of constructing conveyor belt decreases by 3EE and 1DD (min 3EE\n"
+                                       "and 1DD)."
+                },
+
+        }},
+        {2, "Vincent Jeannas", 100, 30, "The cost of constructing cross"
+                                        "decreases by 8EE and 1DD ", .effects = {
+                {
+                        .mode = CONSTRUCTION,
+                        .onOther = false,
+                        .what.machine = MS_CROSS,
+                        .modifierE = -8,
+                        .modifierDD = -1,
+                        .min_costE = 8,
+                        .min_costDD = 1,
+                        .description = "The cost of constructing cross decreases by 8EE and 1DD (min 8EE\n"
+                                       "and 1DD)."
+                },
+
+        }},
+        {2, "Kevin Goilard", 100, 30, "The cost of constructing conveyor belt"
+                                      "decreases by 3EE and 1DD ", .effects = {
+                {
+                        .mode = CONSTRUCTION,
+                        .onOther = false,
+                        .what.machine = MS_CONVEYOR_BELT,
+                        .modifierE = -3,
+                        .modifierDD = -1,
+                        .min_costE = 3,
+                        .min_costDD = 1,
+                        .description = "The cost of constructing conveyor belt decreases by 3EE and 1DD (min 3EE\n"
+                                       "and 1DD)."
+                },
+
+        }},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
+
 
 #endif //PRIM_STAFF_H
---
+++ headers/data/structures.h
@@ -0,0 +1,21 @@
+/*!
+ * \file structures.h
+ * \author Ramzy ZEBRIR
+ * \version 0.1
+ * \date 26/03/2021
+ * \see effect.h
+ *
+ * Contains structures
+ */
+#ifndef PRIM_STRUCTURES_H
+#define PRIM_STRUCTURES_H
+
+#include "effect.h" // effect.h : information about effect
+
+
+
+
+
+
+
+#endif //PRIM_STRUCTURES_H
