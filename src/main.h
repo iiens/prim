84373ab74e7f0d3df924706909107cf150691437
ZEBRIR Ramzy <ramzyz125@hotmail.fr>
2021-03-26 17:51:18 +0100

    Tâche B.4 Modification de main.h

--- src/main.h
+++ src/main.h
@@ -80,10 +80,10 @@
      */
     void main_destroyMachineAction(Map* map);
 
-
+//todo: mettre les bonnes valeurs pour machine_list
     const MachineInfo machine_list[] = {
             {
-                    MS_COLLECTOR,200,20,1,500,200,60,200,
+                    MS_COLLECTOR,200,20,1,500,200,60,
                     "The collector sends a resource produced by the source to the "
                     "neighboring cell indicated by its orientation. It must be placed "
                     "on a square adjacent to that of the source",
@@ -100,7 +100,7 @@
                                             "et 1DD)."
                      },}
             },
-            {MS_CONVEYOR_BELT,60,20,1,-1,-1,60,200,
+            {MS_CONVEYOR_BELT,60,20,1,-1,-1,60,
                     "A carpet has one exit and three entrances, the resource or waste exits"
                     " 1 turn after entering the conveyor belt",
                     0, .effects = {{
@@ -115,17 +115,17 @@
                                                          "et 1DD)."
                                   },}
             },
-            {MS_CROSS,160,20,1,-1,-1,60,200,
+            {MS_CROSS,160,20,1,-1,-1,60,
                     "A cross is a set of two juxtaposed conveyor belts, allowing two "
                     "lines of conveyor belts to cross. The cross has 2 inlets and 2 outlets "
                     "which depend on the orientation of the cross",
                     0, .effects = {}},
-            {MS_RECYCLING_CENTER,500,40,1,1500,100,100,500,
+            {MS_RECYCLING_CENTER,500,40,1,1500,100,100,
                     "The recycling center is used to store waste. The recycling center"
                     " has 4 entrances and 0 exits. At the start of the game, a recycling "
                     "center can store up to 50 pieces of waste",
                     1, .effects = {}},
-            {MS_JUNKYARD,100,100,1,200,600,100,200,
+            {MS_JUNKYARD,100,100,1,200,600,100,
                     "The recycling center turns waste into a resource. Like the conveyor belt,"
                     " the center has 3 entrances and one exit. The center can store up to 100 "
                     "waste",
