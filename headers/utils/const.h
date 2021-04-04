caf0e9dcd3b3d58e6d02a894caafcca8596dd658
ZEBRIR Ramzy <ramzyz125@hotmail.fr>
2021-04-04 17:18:52 +0200

    Tâche B.4 creation of global const

---
+++ headers/utils/const.h
@@ -0,0 +1,29 @@
+/*!
+ * \file const.h
+ * \author Ramzy ZEBRIR
+ * \version 0.1
+ * \date 04/04/2021
+ *
+ * Contains all consts
+ *
+ */
+#ifndef PRIM_CONST_H
+#define PRIM_CONST_H
+
+    #define DD 100 //!< number of DD at the start of the game
+    #define E 100 //!< number of E at the start of the game
+    #define production_fisaE 4 //!< number of E produced by FISA
+    #define production_fisaDD 4 //!< number of DD produced by FISA
+    #define production_fiseE 1 //!< number of E produced by FISE
+    #define production_fiseDD 1 //!< number of DD produced by FISE
+    #define numberFISE = 5 //!< number of FISE at the start of the game
+    #define numberFISA = 5 //!< number of FISA at the start of the game
+    #define cost_fiseE = 50 //!< cost in E of FISE
+    #define cost_fiseDD = 20 //!< cost in DD of FISE
+    #define cost_fisaE= 50 //!< cost in E of FISA
+    #define cost_fisaDD = 20 //!< cost in DD of FISA
+    #define nbTurnFISA = 2 //!< number of turns before FISA Production 
+    #define nbTurnProductionSource = 10 //!< number of turns before Source Production 
+    #define nbRessourceProductBySource = 3  //!< number of ressource produced by Source
+
+#endif //PRIM_CONST_H
