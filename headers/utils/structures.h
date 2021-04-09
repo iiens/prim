7a42dc942d872cd52915652ad0d3f582c3dbf9f7
ZEBRIR Ramzy <ramzyz125@hotmail.fr>
2021-04-09 09:43:30 +0200

    Tâche B.3 add function in structures.c and update of structures.h

--- headers/utils/structures.h
+++ headers/utils/structures.h
@@ -13,91 +13,98 @@
 #include "../data/effect.h" // effect.h : information about effect
 #include "../data/error.h" // todo: ...
 
-    /*!
-     * \typedef ElementType
-     * \enum ElementType_S
-     * \brief todo : do a comment
-     */
-    typedef enum ElementType_S {
-        NUMBER,
-        TEXT,
-        OBJECT
-    } ElementType;
+/*!
+ * \typedef ElementType
+ * \enum ElementType_S
+ * \brief todo : do a comment
+ */
+typedef enum ElementType_S {
+    NUMBER,
+    TEXT,
+    OBJECT
+} ElementType;
 
-    /*!
-    * \typedef Element
-    * \union Element_S effect.h "headers/data/structures.h"
-    * \brief Represent a list
-    */
-    //todo: documentation
-    typedef struct Element_S {
-        union {
-            int number;
-            char* text;
-            void* object;
-        } content;
-        ElementType type; //!< in witch field we got our value
-    } Element;
+/*!
+* \typedef Element
+* \union Element_S effect.h "headers/data/structures.h"
+* \brief Represent a list
+*/
+//todo: documentation
+typedef struct Element_S {
+    union {
+        int number;
+        char* text;
+        void* object;
+    } content;
+    ElementType type; //!< in witch field we got our value
+} Element;
 
-    typedef struct Couple_S{
-        Element keys;
-        Element values;
-    } Couple;
+typedef struct Couple_S{
+    Element keys;
+    Element values;
+} Couple;
 
-    /*!
-     * \typedef Dictionary
-     * \struct Dictionary_S structures.h "headers/data/structures.h"
-     * \brief Struct which contains a dictionary of ElementType
-     */
-    //todo: documentation
-    typedef struct Dictionary_S {
-        int length;
-        int numberOfElement;
-        Couple* entries;
-    } Dictionary;
+/*!
+ * \typedef Dictionary
+ * \struct Dictionary_S structures.h "headers/data/structures.h"
+ * \brief Struct which contains a dictionary of ElementType
+ */
+//todo: documentation
+typedef struct Dictionary_S {
+    int length;
+    int numberOfElement;
+    Couple* entries;
+} Dictionary;
 
-    typedef struct List_S {
-        Element current;
-        struct List_S* next;
-    } List;
+typedef struct List_S {
+    Element current;
+    struct List_S* next;
+} List;
 
-    // create list
-    List* list_create(Element first);
+// create list
+List* list_create(Element first);
 
-    // at the end
-    ErrorCode list_addElement(List* l, Element e);
-    // advance list => next and return current
-    Element list_next(List** current);
-    // get Element
-    Element list_get(List* list);
-    // destroy
-    ErrorCode list_destroy(List* list);
-    /*!
-     * \fn Dictionary* createDictionary()
-     * @brief Create a Dictionary of ElementType and Element
-     * create an array of structures_tab
-     *
-     *
-     * @return Array of the structures_tab
-     */
-    // todo : do the documentation
-    Dictionary* dictionary_create(int length);
-    ErrorCode dictionary_destroy(Dictionary* d);
-    Element* dictionary_getElement(Dictionary* d, Element key);
-    ErrorCode dictionary_addElement(Dictionary* d, Element key, Element value);
-    Couple* dictionary_getCoupletByIndex(Dictionary *d,int index);
-    ErrorCode dictionary_addCoupleText(Dictionary *d,char* key, char* value);
-    ErrorCode dictionary_addCoupleIntText(Dictionary* d,int key, char* value);
+// at the end
+ErrorCode list_addElement(List* l, Element e);
+// advance list => next and return current
+Element list_next(List** current);
+// get Element
+Element list_get(List* list);
+// destroy
+ErrorCode list_destroy(List* list);
+/*!
+ * \fn Dictionary* createDictionary()
+ * @brief Create a Dictionary of ElementType and Element
+ * create an array of structures_tab
+ *
+ *
+ * @return Array of the structures_tab
+ */
+// todo : do the documentation
+Dictionary* dictionary_create(int length);
+ErrorCode dictionary_destroy(Dictionary* d);
+Element* dictionary_getElement(Dictionary* d, Element key);
+ErrorCode dictionary_addElement(Dictionary* d, Element key, Element value);
+Couple* dictionary_getCoupleByIndex( Dictionary *d, int index);
+ErrorCode dictionary_addCoupleText(Dictionary *d,char* key, char* value);
+ErrorCode dictionary_addCoupleIntText(Dictionary* d,int key, char* value);
+Element* dictionary_getElementText(Dictionary *d, char* key);
+Element* dictionary_getElementInt(Dictionary *d, int key);
 
-    /*!
-     * \fn destroyStructuresTab
-     * @brief Destroy the array structures_tab
-     *
-     * At the end of the game we need to clean all of the map
-     * this function will destroy the array
-     *
-     * @param[in] array of structures_tab
-     * @return a void
-     */
+int dictionary_elementToInt(Element e);
+char* dictionary_elementToText( Element e);
+Element dictionary_elementFromInt(int number);
+Element dictionary_elementFromText( char* text);
+
+/*!
+ * \fn destroyStructuresTab
+ * @brief Destroy the array structures_tab
+ *
+ * At the end of the game we need to clean all of the map
+ * this function will destroy the array
+ *
+ * @param[in] array of structures_tab
+ * @return a void
+ */
 
 #endif //PRIM_STRUCTURES_H
--- src/utils/structure.c
+++ src/utils/structure.c
@@ -11,20 +11,22 @@
 #include <stdio.h>
 #include "../../headers/utils/structures.h"
 
-List *list_create(Element first) {
-    List *l = (List *) malloc(sizeof(List));
+List* list_create( Element first )
+{
+    List* l = (List*) malloc(sizeof(List));
     l->current = first;
     l->next = NULL;
     return l;
 }
 
 // at the end
-ErrorCode list_addElement(List *l, Element e) {
-    List *c = l;
-    if (l->next == NULL) {
+ErrorCode list_addElement( List* l, Element e )
+{
+    List* c = l;
+    if ( l->next == NULL ) {
         l->next = list_create(e);
     } else {
-        while (c->next != NULL) {
+        while ( c->next != NULL ) {
             c = c->next;
         }
         c->next = list_create(e);
@@ -33,138 +35,130 @@ ErrorCode list_addElement(List *l, Element e) {
 }
 
 // advance list => next and return current
-Element list_next(List **current) {
+Element list_next( List** current )
+{
     Element e = (*current)->current;
     *current = (*current)->next;
     return e;
 }
 
 // get Element
-Element list_get(List *list) {
+Element list_get( List* list )
+{
     return list->current;
 }
 
 // destroy
-ErrorCode list_destroy(List *list) {
+ErrorCode list_destroy( List* list )
+{
     //todo: no a real free (check list->next)
-    if (list != NULL)
+    if ( list != NULL )
         free(list);
     return NO_ERROR;
 }
 
-Dictionary *dictionary_create(int length) {
-    Dictionary* dict = (Dictionary *) malloc(sizeof(Dictionary));
+Dictionary* dictionary_create( int length )
+{
+    Dictionary* dict = (Dictionary*) malloc(sizeof(Dictionary));
     dict->length = length;
     dict->numberOfElement = 0;
-    dict->entries = (Couple*) malloc(dict->length * sizeof(Couple));
-
-    Couple c1;
-    c1.keys = (Element) { .type = TEXT, .content.text = "a" };
-    c1.values = (Element) { .type = TEXT, .content.text = "aza" };
-
-    Couple c2;
-    c2.keys = (Element) { .type = TEXT, .content.text = "b" };
-    c2.values = (Element) { .type = TEXT, .content.text = "bza" };
-
-    Couple c3;
-    c3.keys = (Element) { .type = TEXT, .content.text = "c" };
-    c3.values = (Element) { .type = TEXT, .content.text = "cza" };
-
-    dict->entries[0] = c1;
-    dict->entries[1] = c2;
-    dict->entries[2] = c3;
-    dict->numberOfElement = 3;
-
+    dict->entries = (Couple*) malloc(length * sizeof(Couple));
     return dict;
 }
 
-ErrorCode dictionary_destroy(Dictionary *d) {
-    if ( d != NULL ){
+ErrorCode dictionary_destroy( Dictionary* d )
+{
+    if ( d != NULL ) {
         free(d->entries);
         free(d);
     }
     return NO_ERROR;
 }
 
-Element *dictionary_getElement(Dictionary *d, Element key) {
-    for (int i = 0; i < d->numberOfElement; i++) {
-        if (d->entries->keys.type == NUMBER && key.type == NUMBER) {
-            if (d->entries->keys.content.number == key.content.number)
-                return &d[i].entries->values;
-        } else if (d->entries->keys.type == OBJECT && key.type == OBJECT) {
-            if (d->entries->keys.content.object == key.content.object)
-                return &d[i].entries->values;
-        } else if (d->entries->keys.type == TEXT && key.type == TEXT) {
-            if (d->entries->keys.content.text == key.content.text)
-                return &d[i].entries->values;
+Element* dictionary_getElement( Dictionary* d, Element key )
+{
+    for ( int i = 0; i < d->numberOfElement; i++ ) {
+        Couple current = d->entries[i]; //!< current value check
+        // type is different, we can't compare
+        if ( key.type != current.keys.type )
+            continue; //next
+        if ( current.keys.type == NUMBER && key.type == NUMBER ) {
+            if ( current.keys.content.number == key.content.number )
+                return &(d->entries[i].values);
+        } else if ( current.keys.type == OBJECT && key.type == OBJECT ) {
+            if ( current.keys.content.object == key.content.object )
+                return &(d->entries[i].values);
+        } else if ( current.keys.type == TEXT && key.type == TEXT ) {
+            if ( current.keys.content.text == key.content.text )
+                return &(d->entries[i].values);
         }
     }
     return NULL;
 }
 
-ErrorCode dictionary_addElement( Dictionary *d, Element key, Element value ) {
-    Couple c; //!<
-    int foundPosition = -1; //!< where we will insert our Couple in the Dictionary
+ErrorCode dictionary_addElement( Dictionary* d, Element key, Element value )
+{
+    if ( d == NULL )
+        return ERROR; //todo: create and use ERROR_DICTIONARY_NULL
 
     // verify that the key don't exist before insert
-    for ( int i = 0; i < d->numberOfElement ; i++ ) {
+    for ( int i = 0; i < d->numberOfElement; i++ ) {
         Couple current = d->entries[i]; //!< current value check
         // type is different, we can't compare
-        if (key.type != current.keys.type) continue; //next
+        if ( key.type != current.keys.type )
+            continue; //next
         // check equals
-        switch (key.type) {
+        switch ( key.type ) {
             case NUMBER:
                 if ( current.keys.content.number == key.content.number ) {
-                    c = current;
-                    foundPosition = i;
+                    // set value and go back
+                    d->entries[i].values = value;
+                    return NO_ERROR;
                 }
                 break;
             case TEXT:
                 if ( strcmp(current.keys.content.text, key.content.text) == 0 ) {
-                    c = current;
-                    foundPosition = i;
+                    // set value and go back
+                    d->entries[i].values = value;
+                    return NO_ERROR;
                 }
                 break;
             case OBJECT:
                 if ( current.keys.content.object == key.content.object ) {
-                    c = current;
-                    foundPosition = i;
+                    // set value and go back
+                    d->entries[i].values = value;
+                    return NO_ERROR;
                 }
                 break;
         }
     }
 
-    if (foundPosition != -1){
-        // replace element
-        c.values = value;
-        d->entries[foundPosition] = c;
-        return NO_ERROR;
-    }
-
-    // we do not replace a value
-    c.keys = key;
-    c.values = value;
-
+    // else
     // realloc if needed
-    if( d->length == d->numberOfElement ){
+    if ( d->length == d->numberOfElement ) {
         d->length += 5;
-        d->entries = (Couple*) realloc(d->entries,d->length * sizeof(Couple));
+        d->entries = (Couple*) realloc(d->entries, d->length * sizeof(Couple*));
     }
 
-    // add and increase number of elements
-    d->entries[d->numberOfElement] = c;
-    d->numberOfElement++;
+    // set
+    d->entries[d->numberOfElement].keys = key;
+    d->entries[d->numberOfElement].values = value;
+    d->numberOfElement++; //increase
 
     return NO_ERROR;
 }
 
-Couple *dictionary_getCoupleByIndex(Dictionary *d, int index) {
+Couple* dictionary_getCoupleByIndex( Dictionary* d, int index )
+{
     if ( index <= d->numberOfElement )
         return &(d->entries[index]);
     return NULL;
 }
 
-ErrorCode dictionary_addCoupleText(Dictionary *d, char *key, char *value) {
+// convenience methods
+
+ErrorCode dictionary_addCoupleText( Dictionary* d, char* key, char* value )
+{
     Element e_key;
     Element e_value;
 
@@ -176,7 +170,21 @@ ErrorCode dictionary_addCoupleText(Dictionary *d, char *key, char *value) {
     return dictionary_addElement(d, e_key, e_value);
 }
 
-ErrorCode dictionary_addCoupleIntText(Dictionary *d, int key, char *value) {
+ErrorCode dictionary_addCoupleInt( Dictionary* d, int key, int value )
+{
+    Element e_key;// rename name of variable
+    Element e_value;
+
+    e_key.type = NUMBER;
+    e_key.content.number = key;
+
+    e_value.type = NUMBER;
+    e_value.content.number = value;
+    return dictionary_addElement(d, e_key, e_value);
+}
+
+ErrorCode dictionary_addCoupleIntText( Dictionary* d, int key, char* value )
+{
     Element e_key;// rename name of variable
     Element e_value;
 
@@ -188,7 +196,8 @@ ErrorCode dictionary_addCoupleIntText(Dictionary *d, int key, char *value) {
     return dictionary_addElement(d, e_key, e_value);
 }
 
-Element* dictionary_getElementText(Dictionary *d, char* key) {
+Element* dictionary_getElementText( Dictionary* d, char* key )
+{
     // create element
     Element e_key;
     e_key.type = TEXT;
@@ -196,13 +205,58 @@ Element* dictionary_getElementText(Dictionary *d, char* key) {
     return dictionary_getElement(d, e_key);
 }
 
-int main(void){
+Element* dictionary_getElementInt( Dictionary* d, int key )
+{
+    // create element
+    return dictionary_getElement(d, dictionary_elementFromInt(key));
+}
+
+int dictionary_elementToInt( Element e )
+{
+    if ( e.type == NUMBER ) {
+        return e.content.number;
+    }
+    return -1;
+}
+
+char* dictionary_elementToText( Element e )
+{
+    if ( e.type == TEXT ) {
+        return e.content.text;
+    }
+    return "";
+}
+
+Element dictionary_elementFromInt( int number )
+{
+    Element e;
+    e.type = NUMBER;
+    e.content.number = number;
+    return e;
+}
+
+Element dictionary_elementFromText( char* text )
+{
+    Element e;
+    e.type = TEXT;
+    e.content.text = text;
+    return e;
+}
+
+int main( void )
+{
     Dictionary* d = dictionary_create(10);
-    /*dictionary_addCoupleText(d, "t", "joker");
-    dictionary_addCoupleText(d, "t", "maria");
-    dictionary_addCoupleText(d, "aza", "fzdaa");
-    printf("%s\n", dictionary_getElementText(d, "t")->content.text);
-    printf("%s\n", dictionary_getElementText(d, "aza")->content.text);
-    printf("%s\n", dictionary_getElementText(d, "blah blah")->content.text);*/
+    dictionary_addCoupleIntText(d, 10, "aza");
+    dictionary_addCoupleInt(d, 3, 3);
+    dictionary_addCoupleInt(d, 5, 5);
+    dictionary_addCoupleText(d, "toto", "choco");
+
+    dictionary_addElement(d, dictionary_elementFromInt(5), dictionary_elementFromInt(7));
+    dictionary_addElement(d, dictionary_elementFromInt(10), dictionary_elementFromText("ok"));
+
+    printf("%s\n", dictionary_elementToText(*dictionary_getElementInt(d, 10)));
+    printf("%s\n", dictionary_elementToText(*dictionary_getElementText(d, "toto")));
+    printf("%d\n", dictionary_elementToInt(*dictionary_getElementInt(d, 3)));
+    printf("%d\n", dictionary_elementToInt(*dictionary_getElementInt(d, 5)));
     dictionary_destroy(d);
 }
