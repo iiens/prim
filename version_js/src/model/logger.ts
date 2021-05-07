560759d8d89d60ddb84d2fd20fd4e86be7bf1e0a
valentin.dreano <valentin.dreano@laposte.net>
2021-05-07 11:01:54 +0200

    Tache C.5 "Réalisation du logger permettant d'écrire à la console et dans un fichier le niveau de d'information choisie (Pour le moment un seul fichier écrasé à chaque lancement)"

---
+++ version_js/src/model/logger.ts
@@ -0,0 +1,134 @@
+const fs = require('fs');
+
+export class Logger {
+    private static instance : Logger | undefined;
+
+    private readonly _fileName : string;
+
+    private _level : Logger.Levels;
+
+    private constructor() {
+        this._fileName = "./logs/log.json";
+
+        // Écriture des nouvelles données
+        fs.writeFileSync(this._fileName, '{}');
+
+        // Initialisation
+        this._level = Logger.Levels.Warning;
+    }
+
+    static get Instance() : Logger {
+        if (Logger.instance === undefined) {
+            Logger.instance = new Logger();
+        }
+
+        return Logger.instance;
+    };
+
+    set level(value: Logger.Levels) {
+        this._level = value;
+    };
+
+    log(message: string, level: Logger.Levels) {
+        if (level >= this._level) {
+            let env: { datetime: string; level: string; message: string; } | undefined = undefined;
+
+            let currentsDate = new Date();
+            let datetime = currentsDate.getDate() + "/"
+                + (currentsDate.getMonth() + 1) + "/"
+                + currentsDate.getFullYear() + " @ "
+                + currentsDate.getHours() + ":"
+                + currentsDate.getMinutes() + ":"
+                + currentsDate.getSeconds();
+
+            switch (level) {
+                case Logger.Levels.Debug:
+                    console.debug(message);
+                    env = {
+                        datetime: datetime,
+                        level: "Debug",
+                        message: message
+                    }
+                    break;
+                case Logger.Levels.Info:
+                    console.info(message);
+                    env = {
+                        datetime: datetime,
+                        level: "Info",
+                        message: message
+                    }
+                    break;
+                case Logger.Levels.Warning:
+                    console.warn(message);
+                    env = {
+                        datetime: datetime,
+                        level: "Warning",
+                        message: message
+                    }
+                    break;
+                case Logger.Levels.Error:
+                    console.error(message);
+                    env = {
+                        datetime: datetime,
+                        level: "Error",
+                        message: message
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            if (env !== undefined) {
+                // Récupération des données dans le fichier
+                let dataFile: string = fs.readFileSync(this._fileName, 'utf8');
+
+                // Transformation des données en Json
+                let dataJson = JSON.parse(dataFile);
+
+                // Vérification de la présence de Json
+                if (dataJson.messages === undefined) {
+                    dataJson.messages = [];
+                }
+
+                // Ajout des nouvelle données
+                dataJson.messages.push(env);
+
+                // Transformation du tableau en json
+                let dataWrite = JSON.stringify(dataJson);
+
+                // Placement de retour à a ligne après chaque fin d'objet
+                const searchRegExp = new RegExp('},', 'g');
+                dataWrite = dataWrite.replace(searchRegExp, '},\n')
+
+                // Écriture des nouvelles données
+                fs.writeFileSync(this._fileName, dataWrite);
+            }
+        }
+    }
+
+    debug (message: string) {
+        this.log(message, Logger.Levels.Debug);
+    }
+
+    info (message: string) {
+        this.log(message, Logger.Levels.Info);
+    }
+
+    warn (message: string) {
+        this.log(message, Logger.Levels.Warning);
+    }
+
+    error (message: string) {
+        this.log(message, Logger.Levels.Error);
+    }
+}
+
+export namespace Logger {
+    export enum Levels {
+        Debug,
+        Info,
+        Warning,
+        Error,
+        Off
+    }
+}
